ce894eb666b64aaef63032647161290230874286
NeelPatel35 <pneel15999@gmail.com>
2022-07-11 16:43:49 +0530

    Add usestate

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import React from 'react'
-import { View, Text, StatusBar } from 'react-native'
-import {customStyle} from './style';
+import React, { useState } from 'react'
+import { View, Text, StatusBar, StyleSheet, Button, TouchableOpacity } from 'react-native'
+
+// import {customStyle} from './style';
 
 export default function App() {
+  const [counter, setcounter] = useState(0)
+
+  const increment = () => {
+    setcounter(counter + 1)
+  }
+  const decrement = () => {
+    if(counter >0){
+      setcounter(counter - 1)
+    }
+  }
   return (
-    <View style={customStyle.mainContainer}>
-      <StatusBar backgroundColor={"red"} />
-      <Text style={customStyle.text} >Neel</Text>
-      <View style={customStyle.shap}>
-        <Text>Hello ):( </Text>
-      </View>
+    <View style={style.container}>
+      <TouchableOpacity onPress={decrement}>
+        <View style={style.customBtn}>
+          <Text style={style.text}>-</Text>
+        </View>
+      </TouchableOpacity>
+
+      <Text style={{ fontSize: 50 }}>{counter}</Text>
+
+      <TouchableOpacity onPress={increment}>
+        <View style={style.customBtn}>
+          <Text style={style.text}>+</Text>
+        </View>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  customBtn: {
+    border: "2px solid black",
+    backgroundColor: "white",
+    color: "white",
+    height: 50,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10
+  },
+  text: {
+    color: "red",
+  }
+})
 
 
 
