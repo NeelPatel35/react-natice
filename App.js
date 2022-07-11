f1a6ad8c00e97eccac491e7b9e676dcf46db835e
NeelPatel35 <pneel15999@gmail.com>
2022-07-11 17:06:48 +0530

    TextInput and manage state

--- App.js
+++ App.js
@@ -1,94 +1,38 @@
 import React, { useState } from 'react'
-import { View, Text, StatusBar, StyleSheet, Button, TouchableOpacity } from 'react-native'
-
-// import {customStyle} from './style';
+import { View, Text, TextInput, StyleSheet, BackHandler } from 'react-native'
 
 export default function App() {
-  const [counter, setcounter] = useState(0)
+  const [inputBoxValue, setInputBoxValue] = useState("");
 
-  const increment = () => {
-    setcounter(counter + 1)
+  const inutboxChagne = (value) => {
+setInputBoxValue(value)
   }
-  const decrement = () => {
-    if(counter >0){
-      setcounter(counter - 1)
-    }
-  }
+
   return (
-    <View style={style.container}>
-      <TouchableOpacity onPress={decrement}>
-        <View style={style.customBtn}>
-          <Text style={style.text}>-</Text>
-        </View>
-      </TouchableOpacity>
-
-      <Text style={{ fontSize: 50 }}>{counter}</Text>
-
-      <TouchableOpacity onPress={increment}>
-        <View style={style.customBtn}>
-          <Text style={style.text}>+</Text>
-        </View>
-      </TouchableOpacity>
+    <View style={style.mainContainer}>
+      <Text>Hello</Text>
+      <TextInput style={style.inputBox} value={inputBoxValue} onChangeText={(value)=>{inutboxChagne(value)}} />
+      <View style={style.outputView}>
+        <Text>{inputBoxValue}</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const style = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
   },
-  customBtn: {
-    border: "2px solid black",
-    backgroundColor: "white",
-    color: "white",
-    height: 50,
-    width: 50,
-    alignItems: "center",
+  outputView: {
+    flex: 1,
     justifyContent: "center",
-    borderRadius: 10
+    alignItems: "center"
   },
-  text: {
-    color: "red",
+  inputBox: {
+    borderWidth: 2,
+    borderColor: "black",
+    margin: 10,
+    padding: 2,
+    width: "90vw"
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function App() {
-//   return (
-//     <View style={{ flex: 1 }}>
-//       {/* TO SHOW ALL PROPERTY OF STATUSBAR TO PRESS CTR+SPACE */}
-//       <StatusBar backgroundColor={"red"} />
-//       {/* <StatusBar hidden={true} backgroundColor={"red"} /> */}
-//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//         <Text style={{ fontSize: 20, fontWeight: "bold", color: "red" }}>Hello </Text>
-//       </View>
-//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//         <Text>World!</Text>
-//       </View>
-//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//         <Text>Awesome</Text>
-//       </View>
-//     </View>
-//   )
-// }
