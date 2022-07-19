4a34787eb2a499c73ff1fc60a03af073c562093b
NeelPatel35 <pneel15999@gmail.com>
2022-07-19 23:42:56 +0530

    map function flatlist component filter and onPress function with touchableOpacity

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import React, { useState } from 'react'
-import { View, Text, TextInput, StyleSheet, BackHandler } from 'react-native'
+import { View, Text, TextInput, StyleSheet, BackHandler, ScrollView, TouchableOpacity, FlatList } from 'react-native'
 
 export default function App() {
-  const [inputBoxValue, setInputBoxValue] = useState("");
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "A"
+    },
+    {
+      id: 2,
+      name: "B"
+    },
+    {
+      id: 3,
+      name: "C"
+    },
+    {
+      id: 4,
+      name: "D"
+    },
+    {
+      id: 5,
+      name: "E"
+    },
+    {
+      id: 6,
+      name: "F"
+    },
+    {
+      id: 7,
+      name: "G"
+    },
+    {
+      id: 8,
+      name: "H"
+    },
+  ]);
 
-  const inutboxChagne = (value) => {
-setInputBoxValue(value)
+  const handleClick = (id) => {
+    setData((prevData)=>{
+      return prevData.filter((tempData)=>{
+        if(tempData.id != id){
+          return tempData
+        }
+      })
+    })
   }
 
   return (
     <View style={style.mainContainer}>
-      <Text>Hello</Text>
-      <TextInput style={style.inputBox} value={inputBoxValue} onChangeText={(value)=>{inutboxChagne(value)}} />
-      <View style={style.outputView}>
-        <Text>{inputBoxValue}</Text>
-      </View>
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => handleClick(item.id)}>
+              <Text style={style.customText} >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          )
+        }}
+      // numColumns={2}
+      />
+
+
+      {/* <ScrollView>
+      {
+        data.map((listData, index) => {
+          return (
+            <View key={index}>
+              <Text style={style.customText}>
+                {listData.name}
+              </Text>
+            </View>
+          )
+        })
+      }
+      </ScrollView> */}
     </View>
   )
 }
@@ -27,6 +90,18 @@ const style = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  customText: {
+    backgroundColor: "black",
+    color: "white",
+    textAlign: "center",
+    margin: 10,
+    borderRadius: 5,
+    padding: 4,
+    fontSize: 30,
+
+
+
   },
   inputBox: {
     borderWidth: 2,
